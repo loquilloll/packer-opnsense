39b35bdf5af663d5eda635ad2aba45b8f2dfbb87
Alvin Sanchez <asanchez@korewireless.com>
2024-07-07 13:34:42 -0400

    add publish to vagrant cloud

--- build.sh
+++ build.sh
@@ -4,10 +4,17 @@
 
 
 # Remove box from vagrant
-vagrant box remove loquilloll/opnsense --all 
+# vagrant box remove loquilloll/opnsense --all 
+
 # Remove box from virt-manager store
-sudo virsh vol-delete --pool default loquilloll-VAGRANTSLASH-opnsense_vagrant_box_image_0_1720319777_box_0.img
+# VOLUME=$(sudo virsh vol-list default | grep loquilloll-VAGRANTSLASH-opnsense | awk '{print $1}')
+# sudo virsh vol-delete --pool default $VOLUME
+
 # Build box
 packer build opnsense.json.pkr.hcl
+
 # Add new box to vagrant
-vagrant box add output/opnsense.box --name loquilloll/opnsense
+# vagrant box add output/opnsense.box --name loquilloll/opnsense
+
+# Publish to vagrant cloud
+vagrant cloud publish loquilloll/opnsense 1.0.0 libvirt output/opnsense.box --release --force
